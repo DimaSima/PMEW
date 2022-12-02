2a523df405bce43f920e3ad00a22f1e014bc78ea
DimaS <dimasimagin@outlook.com>
2022-12-02 22:09:13 +0100

    add mainwindow

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QTabWidget
-from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QStackedWidget, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QVBoxLayout, QMenu, QToolBar
+
+from PyQt5.QtCore import Qt
 
 
-
-class Window(QWidget):
+class Window(QMainWindow):
     """Main Window."""
 
     def __init__(self, parent=None):
@@ -14,16 +14,33 @@ class Window(QWidget):
         super().__init__(parent)
         self.x = 100
         self.y = 100
-        self.width = 1300
-        self.height = 910
+        self.width = 1100
+        self.height = 810
         self.setGeometry(self.x, self.y, self.width, self.height)
         self.setMinimumSize(self.width, self.height)
         self.setStyleSheet("background:rgb(255,255,255)")
-        self.setWindowTitle('Fahrzeugbuchungs - Tool')
+        self.setWindowTitle('Projekt - Planer')
 
+        self.createMenuBar()
 
         WindowLayout = QVBoxLayout()
         self.setLayout(WindowLayout)
+
+    def createMenuBar(self):
+        menuBar = self.menuBar()
+        self.setMenuBar(menuBar)
+
+        fileMenu = QMenu("&File", self)
+        menuBar.addMenu(fileMenu)
+        editMenu = menuBar.addMenu("&Edit")
+        helpMenu = menuBar.addMenu("&Help")
+
+    def createToolBars(self):
+        fileToolBar = self.addToolBar("File")
+        editToolBar = QToolBar("Edit", self)
+        self.addToolBar(editToolBar)
+        helpToolBar = QToolBar("Help", self)
+        self.addToolBar(Qt.LeftToolBarArea, helpToolBar)
 
 
 
